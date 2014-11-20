a259bb5537c45a2f7558c072eab7cd206de025b9
David McGraw <dlmcgraw07@gmail.com>
2014-11-20 07:30:41 -0600

    hooked our main app up to the data provided from the framework and adjusted layout constraints on the main board

--- dojo-apple-watch/ViewController.swift
+++ dojo-apple-watch/ViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var equitySymbol: UILabel!
+    @IBOutlet weak var equityFullName: UILabel!
+    @IBOutlet weak var equityPrice: UILabel!
+    @IBOutlet weak var equityTargetPrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        XMCRequestDataForAllTargets()
+        self.refreshInterface()
+    }
+
+    // MARK: Interface Updates
+    
+    // As it stands we only care about revealing the first target
+    func refreshInterface() {
+        var data: NSDictionary = XMCRequestDataForTopFourTargetsHit()
+    
+        if let key: String = data.allKeys.first as? String {
+            let info: NSDictionary = data[key] as NSDictionary
+            
+            self.equitySymbol.text = key
+            self.equityFullName.text = info["name"] as? String
+            self.equityPrice.text = info["price"] as? String
+            self.equityTargetPrice.text = info["target"] as? String
+        }
     }
 }
 
