5802e423995d6d7be04f6a6726954a92844a017e
David McGraw <dlmcgraw07@gmail.com>
2015-02-21 19:06:54 -0600

    iOS part in place

--- dojo-apple-watch/AppDelegate.swift
+++ dojo-apple-watch/AppDelegate.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 
+let kClientId = "0a72523a8ac8430f91ca5006ea8d4e95"
+let kCallbackURL = "spotifywatchexample://"
+let kTokenSwapUrl = "http://localhost:1234/swap"
+let kTokenRefreshUrl = "http://localhost:1234/refresh"
+
+let kSessionWasUpdated = "kSessionWasUpdated"
+let kSessionLoginDidSucceed = "kSessionLoginDidSucceed"
+let kSessionLoginDidFail = "kSessionLoginDidFail"
+let kSessionObjectDefaultsKey = "kSessionObjectDefaultsKey"
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         return true
     }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        if SPTAuth.defaultInstance().canHandleURL(url, withDeclaredRedirectURL: NSURL(string: kCallbackURL)) {
+            SPTAuth.defaultInstance().handleAuthCallbackWithTriggeredAuthURL(url, tokenSwapServiceEndpointAtURL: NSURL(string: kTokenSwapUrl), callback: { (error, session) in
+                if error != nil {
+                    println("Authorization Error: \(error.localizedDescription)")
+                } else {
+                    // Store our session away for future usage
+                    let sessionData = NSKeyedArchiver.archivedDataWithRootObject(session)
+                    NSUserDefaults.standardUserDefaults().setObject(sessionData, forKey: kSessionObjectDefaultsKey)
+                    NSUserDefaults.standardUserDefaults().synchronize()
+        
+                    NSNotificationCenter.defaultCenter().postNotificationName(kSessionWasUpdated, object: session)
+                }
+            })
+        }
+        return false
+    }
+    
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
